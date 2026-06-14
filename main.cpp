5657104842395fe89ce5e6146fca472db413bb58
RammusOTP <emmanuelphillips33@gmail.com>
2026-06-14 17:56:46 +0100

    wrote functions to insert and bubble up new inserted value into minheap

--- main.cpp
+++ main.cpp
@@ -17,6 +17,22 @@ public:
 
 bool MinHeap::empty() const { return heap.size(); }
 
+int MinHeap::top() const { return empty() ? 0 : heap[0]; }
+
+void MinHeap::push(int value) {
+  heap.emplace_back(value);
+  bubbleUp(heap[heap.size() - 1]);
+}
+
+void MinHeap::bubbleUp(int index) {
+  int parentIndex{(index - 1) / 2};
+  while (heap[index] < heap[parentIndex] && index != 0) {
+    std::swap(heap[index], heap[parentIndex]);
+    index = parentIndex;
+    parentIndex = (index - 1) / 2;
+  }
+}
+
 int main() {
   MinHeap h;
 
